4635931c43c2db4299b107e5ec4e93741457fc1f
Link4Electronics <link4electronics@gmail.com>
2026-08-26 07:18:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q commander-genius-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/cg.svg
-export DESKTOP=/usr/share/applications/cgenius.desktop
+export ICON=https://gitlab.com/Dringgstein/Commander-Genius/-/raw/master/share/cg.svg?ref_type=heads
+export DESKTOP=https://gitlab.com/Dringgstein/Commander-Genius/-/raw/master/share/cgenius.desktop?ref_type=heads
 export STARTUPWMCLASS=CGeniusExe
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/CGeniusExe
+quick-sharun ./AppDir/bin/CGeniusExe
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
